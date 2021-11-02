79bc39e5f1eb85f9b1be2af7d822a66c6c290c25
lehuy299 <huy.tranhuu@saigontechnology.com>
2021-11-02 10:04:04 +0700

    fix issue #4

--- server.js
+++ server.js
@@ -2,84 +2,20 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const User = require('./model/user')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv')
 dotenv.config()
-
-const JWT_SECRET = process.env.JWT_SECRET;
+const userRouter = require('./routers/user.js');
+const app = express()
 const mongoConnectionString = process.env.MONGO_DB_CONNECTION_STRING || 'mongodb://localhost:27017/login-app-db';
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 
 mongoose.connect(mongoConnectionString, {})
 
-const app = express()
 app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
 
-app.post('/api/login', async (req, res) => {
-	const { username, password } = req.body
-	const user = await User.findOne({ username }).lean()
-
-	if (!user) {
-		return res.json({ status: 'error', error: 'Invalid username/password' })
-	}
-
-	if (await bcrypt.compare(password, user.password)) {
-		// the username, password combination is successful
-
-		const token = jwt.sign(
-			{
-				id: user._id,
-				username: user.username
-			},
-			JWT_SECRET
-		)
-
-		return res.json({ status: 'ok', data: token })
-	}
-
-	res.json({ status: 'error', error: 'Invalid username/password' })
-})
-
-app.post('/api/register', async (req, res) => {
-	const { username, password: plainTextPassword } = req.body
-
-	if (!username || typeof username !== 'string') {
-		return res.json({ status: 'error', error: 'Invalid username' })
-	}
-
-	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
-		return res.json({ status: 'error', error: 'Invalid password' })
-	}
-
-	if (plainTextPassword.length < 5) {
-		return res.json({
-			status: 'error',
-			error: 'Password too small. Should be atleast 6 characters'
-		})
-	}
-
-	const password = await bcrypt.hash(plainTextPassword, 10)
-
-	try {
-		const response = await User.create({
-			username,
-			password
-		})
-		console.log('User created successfully: ', response)
-	} catch (error) {
-		if (error.code === 11000) {
-			// duplicate key
-			return res.json({ status: 'error', error: 'Username already in use' })
-		}
-		throw error
-	}
-
-	res.json({ status: 'ok' })
-})
+app.use('/api', userRouter);
 
 app.listen(port, () => {
 	console.log(`Server up at ${port}`)
