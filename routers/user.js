b55265548bec4037233fca55a0b3b9c0ee2e11cd
lehuy299 <huy.tranhuu@saigontechnology.com>
2021-11-02 11:29:09 +0700

    fix issue #5

--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRouter = express.Router();
 const userHandler = require('../handlers/user.js')
+const userValidation = require('../validation/user.js')
+const { validate, ValidationError, Joi } = require('express-validation');
 
-userRouter.post('/login', userHandler.login)
+userRouter.post('/login',validate(userValidation.login, {}, {}), userHandler.login)
 
-userRouter.post('/register', userHandler.register)
+userRouter.post('/register', validate(userValidation.register, {}, {}), userHandler.register)
 
 module.exports = userRouter;
