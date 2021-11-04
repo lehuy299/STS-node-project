7b9168394bf7e1f53ace02558be370cca5f15ec6
lehuy299 <huy.tranhuu@saigontechnology.com>
2021-11-04 17:18:22 +0700

    Fix duplicate username error

--- handlers/user.js
+++ handlers/user.js
@@ -20,15 +20,15 @@ exports.getEdit = async (req, res) => {
 	const username = req.params.username;
 	const user = await User.findOne({ username: username }).lean();
 
-	
-	
+
+
 	res.render('pages/edit', { user: user });
 }
 
 exports.login = async (req, res) => {
 
 	const { username, password } = req.body
-	
+
 	const user = await User.findOne({ username }).lean()
 
 	if (!user) {
@@ -46,13 +46,13 @@ exports.login = async (req, res) => {
 			JWT_SECRET
 		)
 
-        return res
-            .cookie("token", token, {
-            httpOnly: true,
-            //secure: process.env.NODE_ENV === "production",
-            })
-            .redirect('/api/user/profile/' + username)
-    }
+		return res
+			.cookie("token", token, {
+				httpOnly: true,
+				//secure: process.env.NODE_ENV === "production",
+			})
+			.redirect('/api/user/profile/' + username)
+	}
 
 	res.json({ status: 'error', error: 'Invalid username/password' })
 };
@@ -60,40 +60,22 @@ exports.login = async (req, res) => {
 exports.register = async (req, res) => {
 	const { username, password: plainTextPassword, email, firstName, lastName, dateOfBirth } = req.body
 
-	if (!username || typeof username !== 'string') {
-		return res.json({ status: 'error', error: 'Invalid username' })
-	}
-
-	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
-		return res.json({ status: 'error', error: 'Invalid password' })
-	}
-
-	if (plainTextPassword.length < 5) {
-		return res.json({
-			status: 'error',
-			error: 'Password too small. Should be atleast 6 characters'
-		})
-	}
-
 	const password = await bcrypt.hash(plainTextPassword, 10)
 
-	try {
-		const response = await User.create({
-			username,
-			password,
-			email,
-			firstName, 
-			lastName,
-			dateOfBirth
-		})
-		console.log('User created successfully: ', response)
-	} catch (error) {
-		if (error.code === 11000) {
-			// duplicate key
-			return res.json({ status: 'error', error: 'Username already in use' })
-		}
-		throw error
-	}
+	const isDuplicated = await User.findOne({ username: username })
+
+	if (isDuplicated) return res.render('pages/register', { error: "Username has been created" })
+
+	const response = await User.create({
+		username,
+		password,
+		email,
+		firstName,
+		lastName,
+		dateOfBirth
+	})
+	console.log('User created successfully: ', response)
+
 	res.redirect('/login');
 }
 
@@ -106,8 +88,8 @@ exports.update = async (req, res) => {
 			{ username: username },
 			{
 				$set: {
-					firstName: firstName, 
-					lastName: lastName, 
+					firstName: firstName,
+					lastName: lastName,
 					dateOfBirth: dateOfBirth
 				}
 			}
@@ -128,8 +110,8 @@ exports.profile = async (req, res) => {
 
 exports.logout = (req, res) => {
 	return res
-	  .clearCookie("token")
-	  .status(200)
-	  .json({ message: "Successfully logged out 😏 🍀" })
+		.clearCookie("token")
+		.status(200)
+		.json({ message: "Successfully logged out 😏 🍀" })
 };
 
